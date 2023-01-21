57ccca8faf34c30bc4ab4ead6c5cf7d26ea86c34
dynilath <dynilath@qq.com>
2023-01-21 12:38:45 +0800

    fix: new version activity block

--- src/Message/ChatMessages.ts
+++ src/Message/ChatMessages.ts
@@ -8,15 +8,14 @@ interface ActivityInfo {
 export function ActivityDeconstruct(dict: ChatMessageDictionary): ActivityInfo | undefined {
     let SourceCharacter, TargetCharacter, ActivityGroup, ActivityName;
     for (let v of dict) {
-        if (!v.Tag) continue;
         if (v.Tag === 'TargetCharacter' && v.Text && v.MemberNumber)
             TargetCharacter = { Name: v.Text, MemberNumber: v.MemberNumber };
         else if (v.Tag === 'SourceCharacter' && v.Text && v.MemberNumber)
             SourceCharacter = { Name: v.Text, MemberNumber: v.MemberNumber };
-        else if (v.Tag === 'ActivityGroup' && v.Text)
-            ActivityGroup = v.Text;
-        else if (v.Tag === 'ActivityName' && v.Text)
-            ActivityName = v.Text;
+        else if (v.FocusGroupName)
+            ActivityGroup = v.FocusGroupName;
+        else if (v.ActivityName)
+            ActivityName = v.ActivityName;
     }
     if (SourceCharacter === undefined || TargetCharacter === undefined
         || ActivityGroup === undefined || ActivityName === undefined) return undefined;
