7d31f192220390913ca817a73123fc77c822ddea
dynilath <dynilath@qq.com>
2023-11-17 10:32:15 +0800

    New Data Class

--- src/Data.ts
+++ src/Data.ts
@@ -1,10 +1,227 @@
+import { DebugMode } from "./Definition";
+
+const DefaultValueV1: ResponsiveSettingV1 = {
+    settings: { enable: true },
+    hot: ["n... Nyah♥", "Oooh", "mmmmmh!", "NYyaaA♥"],
+    medium: ["mm", "aaaah", "nyAh♥"],
+    light: ["nyah♥", "Aah!", "mh", "oh!♥", "mh♥"],
+    low: ["", "", "mh", "♥oh♥", "ah", "...♥"],
+    orgasm: ["Nya...Ny...NyaaAAaah!", "Mmmmh... MMmh... Hhhmmmm...", "Oooooh... Mmmmh... OooOOOOh!", "Mmmhnn... Nyhmm... Nyah!"],
+    pain: ["Aie!", "Aoouch!", "Aaaaie!", "Ouch", "Aow"],
+    tickle: ["Hahaha!", "Mmmmhahaha!", "Muhahah...", "Ha!Ha!"],
+};
+
+const DefaultValueV1TriggerActivities = {
+    pain: ["Slap", "Bite", "Spank", "Kick", "Pinch", "SpankItem", "ShockItem"],
+    tickle: ["Tickle", "TickleItem"],
+    masturbate: ["MasturbateHand", "MasturbateFist", "MasturbateFoot", "MasturbateTongue", "MasturbateItem", "PenetrateFast", "PenetrateSlow", "PenetrateItem"],
+}
+
+function getDefaultSettings(): ResponsiveSettingV2 {
+    const message_mapper = (_: string) => { return { type: "message", content: _ } as ResponsiveMessage };
+    const action_mapper = (_: string) => { return { type: "action", content: _ } as ResponsiveMessage };
+
+    const default_personality: ResponsiveItem[] = [{
+        name: "Pain",
+        trigger: {
+            mode: "activity",
+            allow_activities: DefaultValueV1TriggerActivities.pain
+        },
+        messages: DefaultValueV1.pain.map(message_mapper).concat(["PLAYER_NAME feels the pain and frowned."].map(action_mapper))
+    }, {
+        name: "Tickle",
+        trigger: {
+            mode: "activity",
+            allow_activities: DefaultValueV1TriggerActivities.tickle,
+        },
+        messages: DefaultValueV1.tickle.map(message_mapper).concat(["PLAYER_NAME squirms from the tickling."].map(action_mapper))
+    }, {
+        name: "Masturbate",
+        trigger: {
+            mode: "activity",
+            allow_activities: DefaultValueV1TriggerActivities.masturbate,
+        },
+        messages: DefaultValueV1.hot.map(message_mapper)
+    }, {
+        name: "Orgasm",
+        trigger: {
+            mode: "orgasm",
+        },
+        messages: DefaultValueV1.orgasm.map(message_mapper)
+    }, {
+        name: "High Arousal",
+        trigger: {
+            mode: "spicer",
+            min_arousal: 80
+        },
+        messages: DefaultValueV1.hot.map(message_mapper)
+    }, {
+        name: "Mid Arousal",
+        trigger: {
+            mode: "spicer",
+            max_arousal: 80,
+            min_arousal: 50
+        },
+        messages: DefaultValueV1.medium.map(message_mapper)
+    }, {
+        name: "Low Arousal",
+        trigger: {
+            mode: "spicer",
+            max_arousal: 50,
+        },
+        messages: DefaultValueV1.low.map(message_mapper)
+    }];
+
+    return Object.assign({}, {
+        settings: { enabled: true }, active_personality: "Default", personalities: [{
+            name: "Default",
+            responses: default_personality
+        }]
+    });
+}
+
+function isStringArray(data: any): data is string[] {
+    return Array.isArray(data) && data.every(_ => typeof _ === 'string');
+}
+
+function isNumberArray(data: any): data is number[] {
+    return Array.isArray(data) && data.every(_ => typeof _ === 'number');
+}
+
+function isV1Setting(data: any): data is ResponsiveSettingV1 {
+    if (data === undefined) return false;
+    if (typeof data.settings !== "object") return false;
+    if (typeof data.settings.enable !== "boolean") return false;
+    if (!isStringArray(data.hot)) return false;
+    if (!isStringArray(data.medium)) return false;
+    if (!isStringArray(data.light)) return false;
+    if (!isStringArray(data.low)) return false;
+    if (!isStringArray(data.orgasm)) return false;
+    if (!isStringArray(data.pain)) return false;
+    if (!isStringArray(data.tickle)) return false;
+    return true;
+}
+
+function pickV2Setting(data: any): ResponsiveSettingV2 {
+    if (data === undefined) return getDefaultSettings();
+    let ret = {};
+    if (typeof data.settings !== "object" || typeof data.settings.enabled !== "boolean") {
+        Object.assign(ret, { settings: { enabled: true } });
+    } else {
+        Object.assign(ret, { settings: { enabled: data.settings.enabled } });
+    }
+
+    if (typeof data.active_personality === "string") {
+        Object.assign(ret, { active_personality: data.active_personality });
+    } else {
+        Object.assign(ret, { active_personality: null });
+    }
+
+    if (Array.isArray(data.personalities)) {
+        let triggers = data.triggers.map((i: any): ResponsivePersonality | undefined => {
+            if (typeof i !== "object") return undefined;
+            if (typeof i.name !== "string") return undefined;
+            if (!Array.isArray(i.responses)) return undefined;
+
+            let responses = i.responses.map((j: any): ResponsiveItem | undefined => {
+                if (typeof j !== "object") return undefined;
+                if (typeof j.name !== "string") return undefined;
+                if (typeof j.trigger !== "object") return undefined;
+
+                let trigger = ((): ResponsiveTrigger | undefined => {
+                    if (typeof j.trigger.mode !== "string") return undefined;
+                    if (j.trigger.mode === "activity") {
+                        let allow_activities = j.trigger.allow_activities;
+                        let allow_bodyparts = j.trigger.allow_bodyparts;
+                        if (allow_activities !== undefined && !isStringArray(allow_activities)) return undefined;
+                        if (allow_bodyparts !== undefined && !isStringArray(allow_bodyparts)) return undefined;
+                        return { mode: "activity", allow_activities, allow_bodyparts };
+                    } else if (j.trigger.mode === "orgasm") {
+                        return { mode: "orgasm" };
+                    } else if (j.trigger.mode === "spicer") {
+                        let min_arousal = j.trigger.min_arousal;
+                        let max_arousal = j.trigger.max_arousal;
+                        let apply_favorite = j.trigger.apply_favorite;
+                        let allow_ids = j.trigger.allow_ids;
+                        if (min_arousal !== undefined && typeof min_arousal !== "number") return undefined;
+                        if (max_arousal !== undefined && typeof max_arousal !== "number") return undefined;
+                        if (apply_favorite !== undefined && typeof apply_favorite !== "boolean") return undefined;
+                        if (!isNumberArray(allow_ids)) return undefined;
+                        return { mode: "spicer", min_arousal, max_arousal, apply_favorite, allow_ids };
+                    }
+                    return undefined;
+                })();
+                if (trigger === undefined) return undefined;
+
+                if (!Array.isArray(j.messages)) return undefined;
+                let messages = j.messages.map((k: any): ResponsiveMessage | undefined => {
+                    if (typeof k !== "object") return undefined;
+                    if (typeof k.type !== "string") return undefined;
+                    if (k.type !== "action" && k.type !== "message") return undefined;
+
+                    if (typeof k.content !== "string") return undefined;
+
+                    return { type: k.type, content: k.content };
+                }).filter((_: any) => _ !== undefined) as ResponsiveMessage[];
+
+                return { name: j.name, trigger: trigger, messages: messages };
+            }).filter((_: any) => _ !== undefined) as ResponsiveItem[];
+
+            return { name: i.name, responses: responses };
+        }).filter((_: any) => _ !== undefined) as ResponsiveTrigger[];
+        Object.assign(ret, { triggers: triggers });
+    } else {
+        Object.assign(ret, { triggers: [] });
+    }
+
+    return ret as ResponsiveSettingV2;
+}
+
+function V1SettingToV2Setting(data: ResponsiveSettingV1): ResponsiveSettingV2 {
+    let def = getDefaultSettings();
+    def.settings.enabled = data.settings.enable;
+    const v1mapper = (_: string) => { return { type: "message", content: _ } as ResponsiveMessage };
+    def.personalities[0].responses = [
+        { name: "Pain", trigger: { mode: "activity", allow_activities: DefaultValueV1TriggerActivities.pain }, messages: data.pain.map(v1mapper) },
+        { name: "Tickle", trigger: { mode: "activity", allow_activities: DefaultValueV1TriggerActivities.tickle }, messages: data.tickle.map(v1mapper) },
+        { name: "Masturbate", trigger: { mode: "activity", allow_activities: DefaultValueV1TriggerActivities.masturbate }, messages: data.hot.map(v1mapper) },
+        { name: "Orgasm", trigger: { mode: "orgasm" }, messages: data.orgasm.map(v1mapper) },
+        { name: "High Arousal", trigger: { mode: "spicer", min_arousal: 80 }, messages: data.hot.map(v1mapper) },
+        { name: "Mid Arousal", trigger: { mode: "spicer", max_arousal: 80, min_arousal: 50 }, messages: data.medium.map(v1mapper) },
+        { name: "Low Arousal", trigger: { mode: "spicer", max_arousal: 50 }, messages: data.low.map(v1mapper) },
+    ];
+    return def;
+}
+
+function DeserializeData(str: string | undefined): ResponsiveSettingV2 {
+    if (str === undefined) return getDefaultSettings();
+
+    let d = LZString.decompressFromBase64(str);
+    let data = {};
+
+    try {
+        let decoded = JSON.parse(d);
+        data = decoded;
+    } catch { }
+
+    if (isV1Setting(data)) {
+        return V1SettingToV2Setting(data);
+    }
+
+    return pickV2Setting(data);
+}
+
 export class DataManager {
     private static _instance: DataManager | undefined;
-    private initFromNoData: boolean = false;
 
     static init() {
         if (this._instance === undefined)
             this._instance = new DataManager;
+        if (DebugMode) {
+            (window as any)["ShowResponsiveData"] = () => {
+                return this._instance?.modData;
+            }
+        }
     }
 
     static get instance() {
@@ -13,67 +230,18 @@ export class DataManager {
     }
 
     modData: ResponsivePartialSetting = {};
-    mergeData: ResponsiveSolidSetting | undefined;
 
-    static DefaultValue: ResponsiveSolidSetting = {
-        settings: { enable: true },
-        hot: ["n... Nyah♥", "Oooh", "mmmmmh!", "NYyaaA♥"],
-        medium: ["mm", "aaaah", "nyAh♥"],
-        light: ["nyah♥", "Aah!", "mh", "oh!♥", "mh♥"],
-        low: ["", "", "mh", "♥oh♥", "ah", "...♥"],
-        orgasm: ["Nya...Ny...NyaaAAaah!", "Mmmmh... MMmh... Hhhmmmm...", "Oooooh... Mmmmh... OooOOOOh!", "Mmmhnn... Nyhmm... Nyah!"],
-        pain: ["Aie!", "Aoouch!", "Aaaaie!", "Ouch", "Aow"],
-        tickle: ["Hahaha!", "Mmmmhahaha!", "Muhahah...", "Ha!Ha!"],
-    };
-
-    private static ValidateStringList(object: any, key: string) {
-        if (object === undefined || !Array.isArray(object[key])) return [];
-        return (object[key] as any[]).filter(_ => typeof _ === 'string');
-    }
-
-    private static ValidatorItem(key: keyof ResponsiveSolidSetting): [keyof ResponsiveSolidSetting, (d: ResponsivePartialSetting) => any] {
-        return [key, (d: ResponsivePartialSetting): string[] => DataManager.ValidateStringList(d, key)]
-    }
-
-    private static Validator = new Map<keyof ResponsiveSolidSetting, (d: ResponsivePartialSetting) => any>([
-        ["settings", (d: ResponsivePartialSetting): ResponsiveSolidSetting['settings'] => {
-            if (d.settings === undefined || typeof d.settings.enable !== "boolean") return { enable: true };
-            return d.settings;
-        }],
-        DataManager.ValidatorItem('low'),
-        DataManager.ValidatorItem('light'),
-        DataManager.ValidatorItem('medium'),
-        DataManager.ValidatorItem('hot'),
-        DataManager.ValidatorItem('pain'),
-        DataManager.ValidatorItem('orgasm'),
-        DataManager.ValidatorItem('tickle'),
-    ])
-
-    private EncodeDataStr() {
-        let data: { [k: string]: any } = {}
-        for (const k in this.modData) {
-            data[k] = this.modData[k as keyof ResponsiveSolidSetting];
-        }
-        return LZString.compressToBase64(JSON.stringify(data));
+    private EncodeDataStr(): string {
+        return LZString.compressToBase64(JSON.stringify(this.modData));
     }
 
     private DecodeDataStr(str: string | undefined) {
         if (str === undefined) {
-            Object.assign(this.modData, DataManager.DefaultValue);
+            Object.assign(this.modData, getDefaultSettings());
             return;
         }
 
-        let d = LZString.decompressFromBase64(str);
-        let data = {};
-
-        try {
-            let decoded = JSON.parse(d);
-            data = decoded;
-        } catch { }
-
-        DataManager.Validator.forEach((v, k) => {
-            this.modData[k as keyof ResponsiveSolidSetting] = v(data);
-        })
+        Object.assign(this.modData, DeserializeData(str));
     }
 
     ServerStoreData() {
@@ -95,30 +263,14 @@ export class DataManager {
             }
             this.DecodeDataStr(rawData);
         }
-        if (this.mergeData !== undefined) {
-            this.modData.settings = { enable: this.mergeData.settings.enable };
-            if (this.initFromNoData) {
-                const rkeys: (keyof ResponsiveSetting)[] = ['low', 'light', 'medium', 'hot', 'orgasm', 'pain', 'tickle'];
-                for (const t of rkeys) {
-                    this.modData[t] = this.mergeData[t];
-                }
-                this.initFromNoData = false;
-            }
-            this.ServerStoreData();
-        }
     }
 
     get data() {
-        return this.modData as ResponsiveSolidSetting;
+        return this.modData as ResponsiveSettingV2;
     }
 
-    set data(d: ResponsiveSolidSetting) {
+    set data(d: ResponsiveSettingV2) {
         this.modData = d;
-    }
-
-    PushMergeData(data: ResponsiveSolidSetting) {
-        this.mergeData = data;
-        if (Player && Player.OnlineSettings) this.ServerTakeData();
     }
 }
 
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,5 @@
-interface ResponsiveSetting {
+interface ResponsiveSettingV1 {
+    settings: { enable: boolean };
     hot: string[];
     medium: string[];
     light: string[];
@@ -8,9 +9,64 @@ interface ResponsiveSetting {
     tickle: string[];
 }
 
-interface ResponsiveSolidSetting extends ResponsiveSetting {
-    settings: { enable: boolean };
+interface ResponsiveTriggerActivity {
+    mode: "activity";
+    allow_activities?: string[];
+    allow_bodyparts?: string[];
+    allow_ids?: number[];
 }
+
+interface ResponsiveTriggerOrgasm {
+    mode: "orgasm";
+    allow_activities?: string[];
+    allow_bodyparts?: string[];
+}
+
+interface ResponsiveTriggerSpicer {
+    mode: "spicer";
+    min_arousal?: number;
+    max_arousal?: number;
+    apply_favorite?: boolean;
+    allow_ids?: number[];
+}
+
+type ResponsiveTrigger = ResponsiveTriggerActivity | ResponsiveTriggerOrgasm | ResponsiveTriggerSpicer;
+
+interface ResponsiveTriggerType {
+    mode: "activity" | "orgasm" | "spicer";
+    min_arousal?: number;
+    max_arousal?: number;
+    apply_favorite?: boolean;
+    allow_activities?: string[];
+    allow_bodyparts?: string[];
+    allow_ids?: number[];
+}
+
+interface ResponsiveMessage {
+    type: "action" | "message",
+    content: string;
+}
+
+interface ResponsiveItem {
+    name: string;
+    trigger: ResponsiveTrigger;
+    messages: ResponsiveMessage[];
+}
+
+interface ResponsivePersonality {
+    name: string;
+    responses: ResponsiveItem[];
+}
+
+interface ResponsiveSettingV2 {
+    settings: { enabled: boolean };
+    active_personality: string | null;
+    personalities: ResponsivePersonality[];
+}
+
+type ResponsiveSetting = ResponsiveSettingV1 | ResponsiveSettingV2;
+
+interface ResponsiveSolidSetting extends ResponsiveSetting { }
 
 type ResponsivePartialSetting = Partial<ResponsiveSolidSetting>;
 
