986b731a86de102fa0c464e2c69ce77207a5aaef
dynilath <dynilath@qq.com>
2024-01-13 05:59:42 +0800

    Member list dialog

---
+++ src/GUI/Response/TriggerMode/MemberListPopup.ts
@@ -0,0 +1,258 @@
+import { Styles } from "../../../Definition";
+import { GetText } from "../../../i18n";
+import { GUISettingScreen } from "../../GUI";
+import { AGUIItem, IPoint, IRect, WithinRect } from "../../Widgets/AGUI";
+import { TextRoundButton } from "../../Widgets/Button";
+import { ADrawCircleRect, ADrawCricleIconButton, ADrawCricleTextButton, ADrawRoundRect, ADrawTextFit, RoundFramedRect } from "../../Widgets/Common";
+import { InputItem, TextAreaItem } from "../../Widgets/InputText";
+import { PageDial, PageDialBinding } from "../../Widgets/PageDial";
+import { Popup } from "../../Widgets/Popup";
+import { BasicText } from "../../Widgets/Text";
+
+const centerX = 1000;
+const centerY = 500;
+
+const dialogPadding = 35;
+
+const titleFontSize = 36;
+
+const tableInnerSpacing = 5;
+const memberTableTitleHeight = 50;
+const memberTableRowHeight = 50;
+const memberTableColumnWidth = 260;
+
+const roundButtonWidth = 50;
+
+const memberTableRowsPerPage = 5;
+const tableHeight = memberTableTitleHeight + memberTableRowHeight * memberTableRowsPerPage;
+const tableWidth = memberTableColumnWidth * 2;
+
+const pageButtonWidth = 100;
+const pagePanelWidth = 300;
+const pageDisplayWidth = 120;
+
+const buttonWidth = 200;
+const buttonHeight = 50;
+
+const memberTableTotalHeight = tableHeight + tableInnerSpacing + buttonHeight;
+const memberTableTotalWidth = Math.max(tableWidth + (tableInnerSpacing + roundButtonWidth) * 2, pagePanelWidth);
+
+const confirmCancelTotalWidth = buttonWidth * 2 + dialogPadding;
+
+const dialogTotalWidth = Math.max(memberTableTotalWidth, buttonWidth * 2 + dialogPadding);
+const dialogTotalHeight = titleFontSize + dialogPadding + Styles.Input.height + dialogPadding + memberTableTotalHeight + dialogPadding + buttonHeight;
+
+class MemberList extends AGUIItem {
+    private readonly _table: IRect;
+
+    private readonly _page_panel: IRect;
+    private readonly _page_display: IRect;
+
+    private readonly memberList: number[];
+
+    private pageBinding: PageDialBinding;
+
+    private pageDial: PageDial;
+
+    constructor(rect: IRect, memberList: number[]) {
+        super();
+        this.memberList = memberList;
+
+        this._table = {
+            x: rect.x + tableInnerSpacing + roundButtonWidth,
+            y: rect.y,
+            width: tableWidth,
+            height: memberTableTitleHeight + memberTableRowHeight * memberTableRowsPerPage
+        };
+
+        this._page_panel = {
+            x: rect.x + rect.width / 2 - pagePanelWidth / 2,
+            y: rect.y + this._table.height + tableInnerSpacing,
+            width: pagePanelWidth,
+            height: buttonHeight
+        };
+
+        this._page_display = {
+            x: rect.x + rect.width / 2 - pageDisplayWidth / 2,
+            y: this._page_panel.y,
+            width: pageDisplayWidth,
+            height: buttonHeight
+        };
+
+        this.pageBinding = {
+            page: 0,
+            maxPage: 0,
+            update: () => { }
+        };
+
+        this.pageDial = new PageDial(this._page_panel, this.pageBinding);
+    }
+
+    Draw(hasFocus: boolean): void {
+        this.pageBinding.maxPage = Math.ceil(this.memberList.length / memberTableRowsPerPage);
+
+        ADrawRoundRect(this._table, Styles.Dialog.roundRadius);
+
+        MainCanvas.strokeStyle = "Gray";
+        MainCanvas.lineWidth = 2;
+        MainCanvas.beginPath();
+        MainCanvas.moveTo(this._table.x, this._table.y + memberTableTitleHeight);
+        MainCanvas.lineTo(this._table.x + this._table.width, this._table.y + memberTableTitleHeight);
+        MainCanvas.stroke();
+        Array.from({ length: memberTableRowsPerPage }, (_, i) => i).forEach(i => {
+            const y = this._table.y + memberTableTitleHeight + memberTableRowHeight * i;
+            MainCanvas.beginPath();
+            MainCanvas.moveTo(this._table.x, y);
+            MainCanvas.lineTo(this._table.x + this._table.width, y);
+            MainCanvas.stroke();
+        });
+
+        ADrawTextFit({
+            x: this._table.x,
+            y: this._table.y,
+            width: memberTableColumnWidth,
+            height: memberTableTitleHeight
+        }, GetText("CharaInfo::MemberID"));
+
+        ADrawTextFit({
+            x: this._table.x + tableInnerSpacing + memberTableColumnWidth,
+            y: this._table.y,
+            width: memberTableColumnWidth,
+            height: memberTableTitleHeight
+        }, GetText("CharaInfo::Name"));
+
+        Array.from({ length: memberTableRowsPerPage }, (_, i) => i).forEach(i => {
+            const y = this._table.y + memberTableTitleHeight + memberTableRowHeight * i;
+            const width = memberTableColumnWidth;
+            const height = memberTableRowHeight;
+            const member = this.memberList[this.pageBinding.page * memberTableRowsPerPage + i];
+            const deleteRect = {
+                x: this._table.x + tableWidth + tableInnerSpacing,
+                y: y + memberTableRowHeight / 2 - roundButtonWidth / 2,
+                width: roundButtonWidth,
+                height: roundButtonWidth
+            }
+            if (member) {
+                ADrawTextFit({ x: this._table.x, y, width, height }, member.toString());
+                const name = Player?.FriendNames?.get(member) || "???";
+                ADrawTextFit({ x: this._table.x + tableInnerSpacing + memberTableColumnWidth, y, width, height }, name);
+                ADrawCricleIconButton(deleteRect, "trashbin", hasFocus, { hover: "#FF4040" })
+            }
+        });
+
+        this.pageDial.Draw(hasFocus);
+    }
+
+    Click(mouse: IPoint): void {
+        this.pageDial.Click(mouse);
+        Array.from({ length: memberTableRowsPerPage }, (_, i) => i).forEach(i => {
+            const y = this._table.y + memberTableTitleHeight + memberTableRowHeight * i;
+            const member = this.memberList[this.pageBinding.page * memberTableRowsPerPage + i];
+
+            const deleteRect = {
+                x: this._table.x + tableWidth + tableInnerSpacing,
+                y: y + memberTableRowHeight / 2 - roundButtonWidth / 2,
+                width: roundButtonWidth,
+                height: roundButtonWidth
+            }
+
+            if (member && WithinRect(mouse, deleteRect)) {
+                this.memberList.splice(this.memberList.indexOf(member), 1);
+            }
+        });
+    }
+}
+
+export class MemberListPopup extends Popup {
+    private readonly _dialog: IRect;
+    private readonly _title: IPoint;
+    private readonly _input: IRect;
+    private readonly _input_confirm: IRect;
+
+    private readonly _pageTable: IRect;
+
+    private readonly _cancel_button: IRect;
+    private readonly _confirm_button: IRect;
+
+    private readonly title_text: string;
+
+    private readonly source_member_list: number[];
+    private readonly editing_member_list: number[];
+
+    private text_input: InputItem;
+
+    constructor(prev: GUISettingScreen | null, title: string, source: number[]) {
+        super(prev);
+        this.title_text = title;
+        this.source_member_list = source;
+        this.editing_member_list = source.slice();
+
+        this._dialog = {
+            x: centerX - dialogTotalWidth / 2 - dialogPadding,
+            y: centerY - dialogTotalHeight / 2 - dialogPadding,
+            width: dialogTotalWidth + dialogPadding * 2,
+            height: dialogTotalHeight + dialogPadding * 2
+        };
+
+        this._title = { x: centerX, y: centerY - dialogTotalHeight / 2 + titleFontSize / 2 };
+
+        this._input = {
+            x: centerX - dialogTotalWidth / 2,
+            y: centerY - dialogTotalHeight / 2 + titleFontSize + dialogPadding,
+            width: dialogTotalWidth - buttonWidth - dialogPadding,
+            height: Styles.Input.height
+        };
+
+        this._input_confirm = {
+            x: centerX + dialogTotalWidth / 2 - buttonWidth,
+            y: this._input.y + Styles.Input.height / 2 - buttonHeight / 2,
+            width: buttonWidth,
+            height: buttonHeight
+        };
+
+        this._pageTable = {
+            x: centerX - memberTableTotalWidth / 2,
+            y: this._input.y + this._input.height + dialogPadding,
+            width: memberTableTotalWidth,
+            height: memberTableTotalHeight
+        };
+
+        this._cancel_button = {
+            x: centerX + confirmCancelTotalWidth / 2 - buttonWidth,
+            y: centerY + dialogTotalHeight / 2 - buttonHeight,
+            width: buttonWidth,
+            height: buttonHeight
+        };
+
+        this._confirm_button = {
+            x: centerX - confirmCancelTotalWidth / 2,
+            y: this._cancel_button.y,
+            width: buttonWidth,
+            height: buttonHeight
+        };
+
+        this.text_input = new InputItem(this._input, "MemberNumberInput", "", GetText("Input ID"));
+
+        this._items = [
+            new RoundFramedRect(this._dialog, Styles.Dialog.roundRadius, "White"),
+            new BasicText(this._title, this.title_text, { align: "center" }),
+            this.text_input,
+            new TextRoundButton(this._input_confirm, GetText("General::Add"), () => {
+                const member_number = parseInt(this.text_input.text);
+                if (!isNaN(member_number) && member_number > 0 && !this.editing_member_list.includes(member_number)) {
+                    this.editing_member_list.push(member_number);
+                    this.text_input.Clear();
+                }
+            }, () => {
+                const v = parseInt(this.text_input.text);
+                return !isNaN(v) && v > 0 && !this.editing_member_list.includes(v);
+            }),
+            new MemberList(this._pageTable, this.editing_member_list),
+            new TextRoundButton(this._cancel_button, GetText("General::Cancel"), () => this.Exit()),
+            new TextRoundButton(this._confirm_button, GetText("General::Confirm"), () => {
+                this.source_member_list.splice(0, this.source_member_list.length, ...this.editing_member_list);
+                this.Exit();
+            })
+        ]
+    }
+}
--- src/GUI/Response/TriggerTab.ts
+++ src/GUI/Response/TriggerTab.ts
@@ -2,34 +2,27 @@ import { Styles } from "../../Definition";
 import { GetText } from "../../i18n";
 import { AGUIItem, IPoint, IRect, WithinRect } from "../Widgets/AGUI";
 import { ADrawCircleRect, ADrawRoundRect, ADrawTextFit } from "../Widgets/Common";
+import { PageDial, PageDialBinding } from "../Widgets/PageDial";
 import { ResponseMenuState } from "./ResponseMenuState";
 
 
 export class TriggerTab extends AGUIItem {
     private _parent: ResponseMenuState;
 
-    private _active_rect_state: { state: 'idle' | 'focused' | 'active'; value: number; }[];
-
-    private _last_update: number = 0;
-
-    private _page: number = 0;
-    private _max_page: number = 0;
-
     readonly _layout_items: IRect[];
-    readonly _layout_prev_button: IRect;
-    readonly _layout_next_button: IRect;
-    readonly _layout_page_text: IRect;
 
     readonly item_height = 60;
     readonly item_border = 5;
 
     readonly _upper_rect: IRect;
-    readonly _lower_rect: IRect;
+
+    readonly _page_rect: IRect;
+    private _page_binding: PageDialBinding;
+    private pageDial: PageDial;
 
     constructor(parent: ResponseMenuState, rect: IRect) {
         super();
         this._parent = parent;
-        this._last_update = Date.now();
 
         const button_spacing = 5;
 
@@ -40,7 +33,6 @@ export class TriggerTab extends AGUIItem {
         const item_width = rect.width - this.item_border * 2;
         const item_count = Math.max(1, Math.floor((rect.height - button_height - this.item_border * 2) / (this.item_height + button_spacing)));
 
-        this._active_rect_state = Array.from({ length: item_count }, _ => { return { state: 'idle', value: 0 }; });
 
         this._layout_items = Array.from({ length: item_count }, (_, index) => {
             return {
@@ -58,98 +50,40 @@ export class TriggerTab extends AGUIItem {
             height: this.item_border * 2 + item_count * (this.item_height + button_spacing) - button_spacing
         };
 
-        this._lower_rect = {
+        this._page_rect = {
             x: rect.x,
             y: rect.y + rect.height - button_height,
             width: rect.width,
             height: button_height
         };
 
-        this._layout_prev_button = {
-            x: this._lower_rect.x + this.item_border,
-            y: this._lower_rect.y + this.item_border,
-            width: button_width - this.item_border * 2,
-            height: button_height - this.item_border * 2
+        this._page_binding = {
+            page: 0,
+            maxPage: 0,
+            update: () => { }
         };
 
-        this._layout_next_button = {
-            x: this._lower_rect.x + this._lower_rect.width - button_width + this.item_border,
-            y: this._layout_prev_button.y,
-            width: this._layout_prev_button.width,
-            height: this._layout_prev_button.height
-        };
-
-        this._layout_page_text = {
-            x: rect.x + rect.width / 2 - page_text_width / 2,
-            y: this._layout_prev_button.y,
-            width: page_text_width,
-            height: this._layout_prev_button.height
-        };
-
-        this.calPage();
-    }
-
-    calPage() {
-        this._max_page = Math.ceil((this._parent.targetPersona.responses.length + 1) / this._layout_items.length) - 1;
-        this._page = Math.min(this._max_page, this._page);
-    }
-
-    update_state(focus: number) {
-        const delta = Date.now() - this._last_update;
-        this._last_update = Date.now();
-
-        const active = this._parent.targetItem;
-
-        this._active_rect_state.forEach((state, index) => {
-            const idx = index + this._page * this._layout_items.length;
-            if (idx > this._parent.targetPersona.responses.length) {
-                state.state = 'idle';
-                state.value = 0;
-                return;
-            }
-
-            if (focus == index) {
-                state.state = 'focused';
-                state.value = Math.min(1, state.value + delta / 100);
-            } else {
-                state.state = 'idle';
-                state.value = Math.max(0, state.value - delta / 100);
-            }
-
-            if (active && active == this._parent.targetPersona.responses[idx]) {
-                state.state = 'active';
-            }
-        });
+        this.pageDial = new PageDial(this._page_rect, this._page_binding);
     }
 
     Draw(hasFocus: boolean): void {
+        this._page_binding.maxPage = Math.ceil(this._parent.targetPersona.responses.length / this._layout_items.length);
         const mouse = { x: MouseX, y: MouseY };
+
+        this.pageDial.Draw(hasFocus);
+
         ADrawRoundRect(this._upper_rect, this.item_height / 2 + this.item_border, { fill: "White", stroke: "Black" });
-        ADrawCircleRect(this._lower_rect, { fill: "White", stroke: "Black" });
-
-        ADrawCircleRect(this._layout_prev_button, { fill: hasFocus && WithinRect(mouse, this._layout_prev_button) ? Styles.Button.hover : "White", stroke: "none" });
-        ADrawTextFit(this._layout_prev_button, "<");
-
-        ADrawCircleRect(this._layout_next_button, { fill: hasFocus && WithinRect(mouse, this._layout_next_button) ? Styles.Button.hover : "White", stroke: "none" });
-        ADrawTextFit(this._layout_next_button, ">");
-
-        ADrawTextFit(this._layout_page_text, `${this._page + 1}/${this._max_page + 1}`);
-
         let focusing = -1;
-
         if (hasFocus) {
             this._layout_items.forEach((rect, index) => {
                 if (WithinRect(mouse, rect)) focusing = index;
             });
-            this.update_state(focusing);
         }
 
         this._layout_items.forEach((rect, index) => {
-            const state = this._active_rect_state[index];
+            const targetIdx = index + this._page_binding.page * this._layout_items.length;
 
-            const targetIdx = index + this._page * this._layout_items.length;
-
-            if (state.state === 'active') {
+            if (targetIdx == this._parent.targetPersona.index) {
                 ADrawCircleRect(rect, { fill: Styles.Tab.active })
             }
 
@@ -166,23 +100,18 @@ export class TriggerTab extends AGUIItem {
     }
 
     Click(mouse: IPoint): void {
-        if (WithinRect(mouse, this._layout_prev_button)) {
-            this._page = Math.max(0, this._page - 1);
-        } else if (WithinRect(mouse, this._layout_next_button)) {
-            this._page = Math.min(this._max_page, this._page + 1);
-        } else {
-            this._layout_items.forEach((rect, index) => {
-                const targetIdx = index + this._page * this._layout_items.length;
-                if (WithinRect(mouse, rect)) {
-                    if (targetIdx == this._parent.targetPersona.responses.length) {
-                        this._parent.targetPersona.responses.push({ name: "New Response", trigger: { mode: "activity", allow_activities: [] }, messages: [] });
-                        this._parent.targetItem = this._parent.targetPersona.responses[targetIdx];
-                        this.calPage();
-                    } else if (targetIdx < this._parent.targetPersona.responses.length) {
-                        this._parent.targetItem = this._parent.targetPersona.responses[targetIdx];
-                    }
+        this.pageDial.Click(mouse);
+
+        this._layout_items.forEach((rect, index) => {
+            const targetIdx = index + this._page_binding.page * this._layout_items.length;
+            if (WithinRect(mouse, rect)) {
+                if (targetIdx == this._parent.targetPersona.responses.length) {
+                    this._parent.targetPersona.responses.push({ name: GetText("Default::NewResponseName"), trigger: { mode: "activity", allow_activities: [] }, messages: [] });
+                    this._parent.targetItem = this._parent.targetPersona.responses[targetIdx];
+                } else if (targetIdx < this._parent.targetPersona.responses.length) {
+                    this._parent.targetItem = this._parent.targetPersona.responses[targetIdx];
                 }
-            });
-        }
+            }
+        });
     }
 }
