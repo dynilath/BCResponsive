b55c643464c8216004e540ee718d58af74fd84bd
dynilath <dynilath@qq.com>
2024-01-16 06:00:22 +0800

    Update loader

--- src/loader.user.js
+++ src/loader.user.js
@@ -4,10 +4,10 @@
 // @version 1.1
 // @description An anto response script for Bondage Club
 // @author Saki Saotome
-// @match bondageprojects.elementfx.com/*
-// @match www.bondageprojects.elementfx.com/*
-// @match bondage-europe.com/*
-// @match www.bondage-europe.com/*
+// @match bondageprojects.elementfx.com/*/BondageClub/*
+// @match www.bondageprojects.elementfx.com/*/BondageClub/*
+// @match bondage-europe.com/*/BondageClub/*
+// @match www.bondage-europe.com/*/BondageClub/*
 // @icon  https://dynilath.gitlab.io/SaotomeToyStore/favicon.ico
 // @grant none
 // @run-at document-end
@@ -17,6 +17,6 @@
     "use strict";
     const src = `https://dynilath.gitlab.io/SaotomeToyStoreVendor/Responsive/main.js?v=${Date.now()}`;
     const loadScript = (url, okay, failed) => fetch(url).then(r => {if(r.ok) return r.text();
-        else throw new Error("Failed to load script")} ).then(okay).catch(() => { setTimeout(() => {failed(url, okay, failed)}, 5000); });
+        else throw new Error("Failed to load script")} ).then(okay).catch(() => { setTimeout(() => {failed(url, okay, failed)}, 15000); });
     loadScript(src, text => {if (typeof BCResponsive_Loaded === "undefined") eval(text);}, loadScript);
 })();
